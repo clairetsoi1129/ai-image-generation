f06cedf9b60894d7b99ab7954b1094d0816a8b92
Claire Tsoi <clairetsoi1129@gmail.com>
2023-05-03 12:06:39 +0100

    use env to store backend url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -5,7 +5,10 @@ import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
+
 const CreatePost = () => {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
+
   const navigate = useNavigate();
 
   const [form, setForm] = useState({
@@ -28,7 +31,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch(`${baseUrl}/dalle`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +59,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch(`${baseUrl}/post`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
